5793589d65381ade74c94442af1c53fd8eef11e0
yosuasimanjuntak <149427769+yosuasimanjuntak@users.noreply.github.com>
2024-06-11 23:34:46 +0700

    Update process_file.py

--- scripts/process_file.py
+++ scripts/process_file.py
@@ -7,7 +7,7 @@ def process_file(bucket_name, object_name):
     blob = bucket.blob(object_name)
 
     # Download the file's contents
-    contents = blob.download_as_text()
+    #contents = blob.download_as_text()
     print(f'Processing file {object_name} from bucket {bucket_name}')
     print(f'Contents: {contents}')
 
